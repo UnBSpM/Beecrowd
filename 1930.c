a51bec25364c90655222d4a38c7ee9909d82e9fc
UnBSpM <252027375@aluno.unb.br>
2025-10-28 17:46:45 -0300

    1930.c

---
+++ 1930.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int T1, T2, T3, T4;
+    scanf("%d %d %d %d", &T1, &T2, &T3, &T4);
+
+    int total = (T1 + T2 + T3 + T4) - 3;
+
+    printf("%d\n", total);
+
+    return 0;
+}
