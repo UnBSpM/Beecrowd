f71dd2e5b83d25c280c6357b52da3d9095c63f49
UnBSpM <252027375@aluno.unb.br>
2025-10-28 17:45:56 -0300

    1018.c

---
+++ 1018.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    printf("%d\n", N);
+
+    int notas[] = {100, 50, 20, 10, 5, 2, 1};
+    int i, qtd;
+
+    for (i = 0; i < 7; i++) {
+        qtd = N / notas[i];
+        printf("%d nota(s) de R$ %d,00\n", qtd, notas[i]);
+        N = N % notas[i];
+    }
+
+    return 0;
+}
