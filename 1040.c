754265e0da7a1030e8b85fe4af323d5b7ce3c7a6
UnBSpM <252027375@aluno.unb.br>
2025-10-29 17:23:54 -0300

    1040.c

---
+++ 1040.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ 
+int main() {
+ float N1, N2, N3, N4, media, exame, media_f;
+ scanf("%f%f%f%f", &N1, &N2, &N3, &N4);
+ media = (N1*2+N2*3+N3*4+N4*1)/10;
+ printf("Media: %.1f\n", media);
+ if(media>=7.0){
+     printf("Aluno aprovado.\n");
+ } else if(media<5.0){
+     printf("Aluno reprovado.\n");
+ } else{
+     printf("Aluno em exame.\n");
+     scanf("%f", &exame);
+     printf("Nota do exame: %.1f\n", exame);
+     media_f = (media+exame)/2.0;
+     if(media_f>=5.0){
+         printf("Aluno aprovado.\n");
+     }else{
+         printf("Aluno reprovado.\n");
+     } printf("Media final: %.1f\n", media_f);
+ }
+  return 0;
+}
